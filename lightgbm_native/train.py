5b8d3af9ff8ab3fa1458563023fe1d9e3715ad58
Ling Huang <lhuang@themeetgroup.com>
2022-07-14 21:04:18 -0400

    add code

--- lightgbm_native/train.py
+++ lightgbm_native/train.py
@@ -41,9 +41,7 @@ def main():
 
     # prepare train and test data
     iris = datasets.load_iris()
-    X = iris.data
-    y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
 
     # enable auto logging
     mlflow.lightgbm.autolog()
